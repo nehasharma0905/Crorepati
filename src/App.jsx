7c777c73ba6ae38ec2128fb24d56556041201d02
Anshul <tilondiya.1@iitj.ac.in>
2024-06-02 17:50:01 +0530

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
-import { useEffect } from "react";
+import { Box } from "@mui/material";
 import { AppRouter } from "./AppRouter";
 import "./styles/style.scss";
-import { Box } from "@mui/material";
-import { testApi } from "./api/sampleApi";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase/firebase";
-import { useDispatch, useSelector } from "react-redux";
-import { usersAction } from "./redux/userSlice";
 
 function App() {
-  const dispatch = useDispatch();
-  const {isUserLoggedIn} = useSelector((state) => state.users);
-  useEffect(() => {
-    if (isUserLoggedIn) {
-      console.log("isUserLoggedIn", isUserLoggedIn);
-      testApi().then((res) => {
-        console.log(res);
-      });  
-    }
-  }, [isUserLoggedIn]);
-
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log("user", user);
-        dispatch(usersAction.updateLoginStatus({ isLoggedIn: true, user: user }));
-      }
-    });
-  },[dispatch])
-
-
 
   return (
     <Box className="App">
