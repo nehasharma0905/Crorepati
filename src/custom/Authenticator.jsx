ec4f5564fb4a81ddb3ff436380ebc7845b517de2
Anshul <tilondiya.1@iitj.ac.in>
2024-06-02 18:29:43 +0530

    wip

--- src/custom/Authenticator.jsx
+++ src/custom/Authenticator.jsx
@@ -1,3 +1,14 @@
+import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+
 export const Authenticator = () => {
-  
+    const { isLoading, userObject, isUserLoggedIn } = useSelector((state) => state.users);
+    
+    return (
+        isUserLoggedIn && userObject ? (<Outlet />) : isLoading ? (
+            <div>Loading...</div>
+        ) : (
+            <div>Not Authorized</div>
+        )
+    )
 }
